90554e6175a600e16f6caf78c5b22c08694ad027
Jaziell SIlva <Jaziell_sic@tesco.edu.mx>
2022-10-10 13:19:25 -0500

    SH Actualizado

--- Menu.sh
+++ Menu.sh
@@ -21,12 +21,16 @@ while :
 	echo -OpenJDK/OpenJRE8
 	echo -Python3
 	echo -MySQL
-	echo Analizando sistema && uname --a && sleep 3
-	echo Analisis completo && echo Fecha inicio: &&  date && sleep 3
+	echo Analizando sistema && uname --a && sleep 2
+	echo Analisis completo && echo Fecha inicio: &&  date && sleep 2
 	echo Actualizacion iniciada
+	#ACTUALIZACION DE SISTEMA
 	sudo apt-get update -y
 	sudo apt-get upgrade -y
+	sudo apt full-upgrade
+	sudo apt clean
 	echo ACTUALIZACION DE SISTEMA FINALIZADA
+	#INSTALACION DE PAQUETES
 	echo INSTALACION DE PAQUETES INICIADA
 	REQUIRED_PKG="openjdk-11-jdk"
 	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
@@ -51,9 +55,34 @@ while :
 	echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
 	sudo apt install mariadb-server --yes  $REQUIRED_PKG
 	fi
-
 	echo Instalacion finalizada, por favor espere && sleep 5
+	#CONFIGURACION SQL
 	echo Inicializando configuracion SQL && sleep 3
+	sudo mysql -u root -e "CREATE USER 'TEST11'@'localhost' IDENTIFIED BY '1234';"
+	sudo mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'TEST11'@'localhost'"
+	sudo mysql -u root -e "FLUSH PRIVILEGES;"
+	sudo mysql -u root -e "CREATE DATABASE reciclador_local;"
+	sudo mysql -u root reciclador_local <bd_Reciclador.sql
+	sudo mysql -u root -e "SHOW DATABASES;"
+	sudo mysql -u root -e "SHOW TABLES FROM reciclador_local;"
+	echo "VERIFIQUE DATOS POR SEGURIDAD" && sleep 3
+	#CONFIGURACION RECICLADORA
+	echo CONFIGURACION AGREGAR RECICLADORA
+    echo LEA CON CUIDADO LAS INSTRUCCIONES DE NO HACERLO PUEDE GENERAR UNA CONFIGURACION INCORRECTA
+	echo A CONTINUACION  SE LE SOLICITARAN CIERTOS DATOS PARA PODER CONFIGURAR SU RECICLADOR
+	echo "Contraseña" Aqui deberas ingresar la contraseña para el reciclador donde estas configurando
+	echo "Ubicacion" Aqui deberas Ingresar la ubicacion de la recicladora para agilizar su ubicacion
+	echo "Nombre" Aqui deberas asignarle un nombre a la recicladora en cuestion
+	echo En caso de requerir un espacio usa un guion bajo _ [SOLO APLICA EN LOS CAMPOS UBICACION Y NOMBRE]
+ 	echo EN CASO DE COMETER UN ERROR PULSE CONTROL+C PARA CANCELAR
+	sleep 3s
+	read -p "Contrasena: " var_1
+	read -p "Ubicacion: " var_2
+	read -p "Nombre_Reciclador: " var_3
+	java -jar AgenteAgregar.jar agregar $var_1 $var_2 $var_3
+	history -c     
+	
+		
         echo "Presione Enter para regresar al menu" ; read;;
       2) 
         echo *Por favor espere. Instalando
@@ -97,15 +126,16 @@ while :
 	echo Instalacion finalizada, por favor espere && sleep 5
         echo "Presione Enter para regresar al menu" ; read;;
 	  4)
-		#COMANDOS SQL
-        sudo mysql -u root -e "CREATE USER 'TEST11'@'localhost' IDENTIFIED BY '1234';"
-        sudo mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'TEST11'@'localhost'"
-        sudo mysql -u root -e "FLUSH PRIVILEGES;"
-        sudo mysql -u root -e "CREATE DATABASE reciclador_local;"
-        sudo mysql -u root reciclador_local <bd_Reciclador.sql
-        sudo mysql -u root -e "SHOW DATABASES;"
-        sudo mysql -u root -e "SHOW TABLES FROM reciclador_local;"
-        echo "VERIFIQUE DATOS POR SEGURIDAD" && sleep 3
+	#COMANDOS SQL
+	sudo mysql -u root -e "CREATE USER 'TEST999'@'localhost' IDENTIFIED BY '1234';"
+	sudo mysql -u root -e "GRANT ALL PRIVILEGES ON * . * TO 'TEST11'@'localhost'"
+	sudo mysql -u root -e "FLUSH PRIVILEGES;"
+	sudo mysql -u root -e "CREATE DATABASE reciclador_local;"
+	sudo mysql -u root reciclador_local <bd_Reciclador.sql
+	sudo mysql -u root -e "SHOW DATABASES;"
+	sudo mysql -u root -e "SHOW TABLES FROM reciclador_local;"
+	echo "VERIFIQUE DATOS POR SEGURIDAD" && sleep 3
+	
 
 		echo "Presione Enter para regresar al menu" ; read;;
       5)
